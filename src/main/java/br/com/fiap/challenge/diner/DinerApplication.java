776a54adbd4aa21a2d2b9625382352c30600ebed
dedeus6 <filipedeus2015@gmail.com>
2024-10-16 21:34:03 -0300

    Initial Configs

---
+++ src/main/java/br/com/fiap/challenge/diner/DinerApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.challenge.diner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/challenge/diner/DinerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.challenge.diner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
