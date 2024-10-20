561a0254606713a487c938792c527a410a5c80c9
Gustavo Biasi <gustavo.biasi@grupoimpulse.com.br>
2024-10-20 02:10:03 -0300

    ajuste application e add scripts

---
+++ src/main/resources/script.sql
@@ -0,0 +1,89 @@
+CREATE TABLE IF NOT EXISTS public.empresa (
+id bigserial NOT NULL,
+razao_social TEXT,
+nome_fantasia TEXT,
+cnpj TEXT,
+ativo TEXT,
+CONSTRAINT empresa_pkey PRIMARY KEY (id)
+);
+
+COMMENT ON COLUMN public.empresa.ativo IS 'S=SIM/N=NAO';
+
+CREATE TABLE IF NOT EXISTS public.forma_pagamento (
+id bigserial NOT NULL,
+descricao TEXT,
+tipo_pagamento TEXT,
+CONSTRAINT forma_pagamento_pkey PRIMARY KEY (id)
+);
+
+COMMENT ON COLUMN public.forma_pagamento.tipo_pagamento IS 'AVISTA/DEBITO/CREDITO/PIX';
+
+CREATE TABLE IF NOT EXISTS public.cliente (
+id bigserial NOT NULL,
+nome TEXT NOT NULL,
+cnpj TEXT NOT NULL,
+telefone TEXT,
+email TEXT,
+CONSTRAINT cliente_pkey PRIMARY KEY (id)
+);
+
+CREATE INDEX idx_cliente_nome ON public.cliente USING btree (nome);
+
+CREATE TABLE IF NOT EXISTS public.categoria (
+id bigserial NOT NULL,
+descricao TEXT NOT NULL,
+CONSTRAINT categoria_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS public.produto (
+id bigserial NOT NULL,
+descricao TEXT NOT NULL,
+vlr_unitario NUMERIC(15,2),
+codigo_barras TEXT,
+categoria_id bigint,
+CONSTRAINT produto_pkey PRIMARY KEY (id),
+CONSTRAINT fk_produto_categoria_id FOREIGN KEY (categoria_id) REFERENCES public.categoria(id)
+);
+
+CREATE TABLE IF NOT EXISTS public.pedido (
+id bigserial NOT NULL,
+data timestamp NOT NULL,
+empresa_id bigint NOT NULL,
+cliente_id bigint,
+status TEXT NOT NULL DEFAULT 'R',
+CONSTRAINT pedido_pkey PRIMARY KEY (id),
+CONSTRAINT fk_pedido_empresa_id FOREIGN KEY (empresa_id) REFERENCES public.empresa(id),
+CONSTRAINT fk_pedido_cliente_id FOREIGN KEY (cliente_id) REFERENCES public.cliente(id)
+);
+
+COMMENT ON COLUMN public.pedido.status IS 'R=RECEBIDO/E=EM PREPARAÇÃO/P=PRONTO/F=FINALIZADO';
+
+
+CREATE TABLE IF NOT EXISTS public.pedido_pagamento (
+id bigserial NOT NULL,
+forma_pagamento_id bigint NOT NULL,
+pedido_id bigint NOT NULL,
+vlr_pagamento NUMERIC(12,2) NOT NULL,
+confirmado TEXT NOT NULL DEFAULT 'N',
+observacao TEXT,
+CONSTRAINT pedido_pagamento_pkey PRIMARY KEY (id),
+CONSTRAINT fk_pedido_pagamento_forma_pagamento_id FOREIGN KEY (forma_pagamento_id) REFERENCES public.forma_pagamento(id),
+CONSTRAINT fk_pedido_pagamento_pedido_id FOREIGN KEY (pedido_id) REFERENCES public.pedido(id)
+);
+
+COMMENT ON COLUMN public.pedido_pagamento.confirmado IS 'S=SIM/N=NAO';
+
+CREATE TABLE IF NOT EXISTS public.pedido_item (
+id bigserial NOT NULL,
+produto_id bigint NOT NULL,
+pedido_id bigint NOT NULL,
+qtd_produto bigint,
+vlr_unitario NUMERIC(15,2) DEFAULT 0.0 NOT NULL,
+vlr_desconto NUMERIC(15,2) DEFAULT 0.0 NOT NULL,
+vlr_bruto NUMERIC(15,2) DEFAULT 0.0 NOT NULL,
+vlr_liquido NUMERIC(15,2) DEFAULT 0.0 NOT NULL,
+observacao TEXT,
+CONSTRAINT pedido_item_pkey PRIMARY KEY (id),
+CONSTRAINT fk_pedido_item_produto_id FOREIGN KEY (produto_id) REFERENCES public.produto(id),
+CONSTRAINT fk_pedido_item_pedido_id FOREIGN KEY (pedido_id) REFERENCES public.pedido(id)
+);
